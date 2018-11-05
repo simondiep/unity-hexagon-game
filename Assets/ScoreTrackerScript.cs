441a2b5dd5e0868806da0c655f286abe7d96a3d0
Simon Diep <simondiep1@gmail.com>
2018-11-05 11:53:23 -0600

    Added high score tracker

--- Assets/ScoreTrackerScript.cs
+++ Assets/ScoreTrackerScript.cs
@@ -5,17 +5,26 @@ using UnityEngine.UI;
 
 public class ScoreTrackerScript : MonoBehaviour {
 	public Text scoreText;
+	public Text highScoreText;
 	private int score;
 	
 	// Use this for initialization
 	void Start () {
 		score = 0;
 		scoreText.text = "Score: " + score.ToString();
+		int highScore = PlayerPrefs.GetInt("highScore", 0);
+		highScoreText.text = "High Score: " + highScore.ToString();
 		SharedRefs.scoreTrackerScript = this;
 	}
 
 	public void incrementScore() {
 		score++;
 		scoreText.text = "Score: " + score.ToString();
+		int highScore = PlayerPrefs.GetInt("highScore", 0);
+		if(highScore <= score)
+		{
+				PlayerPrefs.SetInt("highScore", score);
+				highScoreText.text = "High Score: " + highScore.ToString();
+		}
 	}
 }
